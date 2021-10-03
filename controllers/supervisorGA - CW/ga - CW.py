80b5c2902ee5347cb4a85a87ce3ee50dbaad5020
Jhielson M. Pimentel <J.Montino-Pimentel@sms.ed.ac.uk>
2021-10-03 14:48:15 +0100

    Update GA.py

--- controllers/supervisorGA - CW/ga - CW.py
+++ controllers/supervisorGA - CW/ga - CW.py
@@ -2,7 +2,7 @@ import numpy,random
 
 def population_reproduce(genotypes,elite):
     ### Define crossover rate (integer number between 0 and 100)
-    # cp = ? 
+    cp = ?
     genotypes_not_ranked = genotypes
     # Rank: lowest to highest fitness
     genotypes = rankPopulation(genotypes)
@@ -71,7 +71,7 @@ def mutation(child):
     # Changes a single gene randomly
     after_mutation = []
     ### Define mutation percentage (integer number between 0 and 100)
-    #mp = ? 
+    mp = ?
     for gene in range(len(child)):
         if(random.randint(1,100) < mp):
             # The random value to be added to the gene
